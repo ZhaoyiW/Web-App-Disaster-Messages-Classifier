fe9e6e292ef4a4560324c67ed71c0b7b7dce6cfa
Zhaoyi Wang <zhaoyiw@brandeis.edu>
2021-01-14 11:23:30 -0500

    Add docstrings for functions

--- data/process_data.py
+++ data/process_data.py
@@ -4,6 +4,17 @@ import re
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
+    '''
+    Load datasets and merge into one dataframe
+
+        Parameters:
+                messages_filepath (str): Filepath of the messages dataset
+                categories_filepath (str): Filepath of the categories dataset
+
+        Returns:
+                df (pandas.DataFrame): A merged dataframe of the two datasets
+    '''
+    # Read datasets
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     # Merge the two datasets using the common id
@@ -12,11 +23,21 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
-    # Split the values in the categories column
+    '''
+    Split the categories column into separate, clearly named columns, 
+    convert values to binary, and drop duplicates.
+
+        Parameters:
+                df (pandas.DataFrame): The returned dataframe from load_data()
+        
+        Returns:
+                df (pandas.DataFrame): A cleaned dataframe
+    '''
+    # Split the categories column into separate columns
     categories = df['categories'].str.split(';', expand=True)
     # Extract a list of new column names for categories
     colnames = re.sub(r"-[0-9]", "", df["categories"][0]).split(";")
-    # rename the columns of `categories`
+    # Rename the columns of `categories`
     categories.columns = colnames
     
     # Convert category values to 0 or 1
@@ -26,17 +47,26 @@ def clean_data(df):
         # Convert column from string to numeric
         categories[column] = pd.to_numeric(categories[column])
     
-    # drop the original categories column
+    # Drop the original categories column
     df.drop('categories', axis=1, inplace=True)
-    # concatenate the original dataframe with the new 'categories' dataframe
+    # Concatenate the original dataframe with the new 'categories' dataframe
     df = pd.concat([df, categories], axis=1)    
     
-    # drop duplicates
+    # Drop duplicates
     df.drop_duplicates(inplace=True)
     
     return df
 
 def save_data(df, database_filename):
+    '''
+    Save the dataset into a sqlite database
+
+        Parameters:
+                df (pandas.DataFrame): The cleaned dataset returned from clean_data()
+                database_filename (str): Name of the database
+        
+        No returns
+    '''
     engine = create_engine('sqlite:///{}'.format(database_filename))
     df.to_sql('Response', engine, index=False)
 
@@ -45,14 +75,14 @@ def main():
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
-
+        # Load and merge datasets
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
         df = load_data(messages_filepath, categories_filepath)
-
+        # Clean the dataset
         print('Cleaning data...')
         df = clean_data(df)
-        
+        # Save the dataset into a sqlite database
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
         
